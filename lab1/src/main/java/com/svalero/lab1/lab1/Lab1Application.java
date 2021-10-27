0d2579f38f25e66d385cdff07ac2e92b1190d340
Alberto Vicente Bernad <avicenteb@hiberus.com>
2021-10-27 20:20:17 +0200

    Spring Initializr

---
+++ lab1/src/main/java/com/svalero/lab1/lab1/Lab1Application.java
@@ -0,0 +1,13 @@
+package com.svalero.lab1.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Application.class, args);
+	}
+
+}
